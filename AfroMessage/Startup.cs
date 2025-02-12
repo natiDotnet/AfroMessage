c75ca5c1bae7dab5f2d39052c42a8224a1bf74ab
Natnael Yirga <NatiDotnet@gmail.com>
2025-02-12 14:11:29 +0000

    fix: dependency fail

--- AfroMessage/Startup.cs
+++ AfroMessage/Startup.cs
@@ -18,11 +18,10 @@ public static class Startup
         services.AddOptions<AfroMessageConfig>().BindConfiguration(AfroMessageConfig.Key);
         services
             .AddHttpClient<IAfroMessageClient>()
-            .AddTypedClient((client, sp) =>
+            .AddTypedClient<IAfroMessageClient>((client, sp) =>
             {
                 config ??= sp.GetRequiredService<IOptions<AfroMessageConfig>>().Value;
                 client.BaseAddress = new Uri(AfroMessageConfig.Url);
-                //client.DefaultRequestHeaders.Add("Authorization", $"Bearer {config.Token}");
                 return new AfroMessageClient(config, client);
             })            
             .AddHttpMessageHandler<ConfigDelegatingHandler>()
